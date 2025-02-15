6e7670e681d096092662b27a517f585139d16861
MihneaTs1 <103445201+MihneaTs1@users.noreply.github.com>
2025-02-15 15:19:28 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,44 +1,53 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+
 -- Plugins
 
 vim.cmd([[
+
 call plug#begin()
-  
+
 Plug 'm4xshen/autoclose.nvim'
 Plug 'folke/tokyonight.nvim'
 Plug 'MunifTanjim/nui.nvim'
 Plug 'xeluxee/competitest.nvim'
 Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'nvim-tree/nvim-tree.lua'
 
 call plug#end()
 PlugInstall
 q
+
 ]])
 
 -- Options
 
 require("autoclose").setup()
 require("competitest").setup {
-        received_problems_path = "$(HOME)/Documents/Programming/Competitive/$(JUDGE)/$(CONTEST)/$(PROBLEM).$(FEXT)",
+	received_problems_path = "$(HOME)/Documents/Programming/Competitive/$(JUDGE)/$(CONTEST)/$(PROBLEM).$(FEXT)",
 	received_contests_directory = "$(HOME)/Documents/Programming/Competitive/$(JUDGE)/$(CONTEST)",
- 	received_contests_problems_path = "$(PROBLEM)/main.$(FEXT)",
-        compile_command = {
-                cpp = { exec = "g++", args = { "-std=gnu++23", "-Wall", "-DLOCAL", "$(FNAME)", "-o", "$(FNOEXT)" } },
-        },
+	received_contests_problems_path = "$(PROBLEM)/main.$(FEXT)",
+	compile_command = {
+		cpp = { exec = "g++", args = { "-std=gnu++23", "-Wall", "-DLOCAL", "$(FNAME)", "-o", "$(FNOEXT)" } },
+	},
 
-        testcases_directory = "./.competitest",
-        template_file = "$(HOME)/Documents/Programming/Competitive/Template/template.cpp",
-        evaluate_template_modifiers = true,
+	testcases_directory = "./.competitest",
+	template_file = "$(HOME)/Documents/Programming/Competitive/Template/template.cpp",
+	evaluate_template_modifiers = true,
 }
 require("nvim-treesitter.configs").setup {
-        highlight = {
-                enable = true,
-        },
+	highlight = {
+		enable = true,
+	},
 }
+require("nvim-tree").setup()
 
 vim.o.tabstop=4
 vim.o.shiftwidth=4
 vim.o.number=true
 vim.cmd([[
-colorscheme tokyonight-night
-set clipboard=unnamedplus
+	colorscheme tokyonight-night
+	set clipboard=unnamedplus
+	NvimTreeOpen
 ]])
