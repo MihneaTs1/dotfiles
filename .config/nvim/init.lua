e0d8e690e336d08a5e871813bf16bbe486703ada
MihneaTs1 <103445201+MihneaTs1@users.noreply.github.com>
2025-02-16 14:54:32 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,6 +16,9 @@ Plug 'nvim-tree/nvim-tree.lua'
 Plug 'folke/tokyonight.nvim'
 Plug 'nvim-lualine/lualine.nvim'
 Plug 'nvim-tree/nvim-web-devicons'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvimdev/dashboard-nvim'
 
 call plug#end()
 PlugInstall
@@ -45,6 +48,64 @@ require("nvim-treesitter.configs").setup {
 }
 require("nvim-tree").setup()
 require("lualine").setup()
+require('dashboard').setup({
+  theme = 'doom',
+  config = {
+    header = {
+	  "",
+	  "",
+ 	  ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+	  ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+	  ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+	  ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+	  ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+	  ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+      "",
+	  "",
+  },
+    center = {
+      {
+        icon = ' ',
+        icon_hl = 'Title',
+        desc = 'Find File',
+        desc_hl = 'String',
+        key = 'f',
+        keymap = 'Telescope find_files',
+        key_hl = 'Number',
+        key_format = ' %s',  -- This removes the default surrounding brackets
+        action = 'Telescope find_files',
+      },
+      {
+        icon = ' ',
+        icon_hl = 'Title',
+        desc = 'Recent Files',
+        desc_hl = 'String',
+        key = 'r',
+        keymap = 'Telescope oldfiles',
+        key_hl = 'Number',
+        key_format = ' %s',
+        action = 'Telescope oldfiles',
+      },
+      {
+        icon = ' ',
+        icon_hl = 'Title',
+        desc = 'Edit Config',
+        desc_hl = 'String',
+        key = 'c',
+        keymap = 'edit ~/.config/nvim/init.lua',
+        key_hl = 'Number',
+        key_format = ' %s',
+        action = 'edit ~/.config/nvim/init.lua',
+      },
+    },
+    footer = {
+      "Happy Coding!",
+      "https://github.com/nvimdev/dashboard-nvim",
+    },
+  },
+})
+require("telescope").setup()
+
 
 vim.o.tabstop=4
 vim.o.shiftwidth=4
@@ -52,5 +113,4 @@ vim.o.number=true
 vim.cmd([[
 	set clipboard=unnamedplus
 	colorscheme tokyonight-night
-	NvimTreeOpen
 ]])
